84f6e66d8c54d3ec25aa7c113f80e33179c12899
Paulo Henrique <paulohenriquerev@gmail.com>
2020-02-01 19:33:57 -0300

    Used formik and yup for validate the pages SignIn/SignUp

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,15 +1,54 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Container, Button, Input } from "./styles";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { Container, Button, Input, MessageError } from "./styles";
+
+const FormSchema = Yup.object().shape({
+  username: Yup.string().required("Campo nome é obrigatório!"),
+  email: Yup.string()
+    .email("Insira um e-mail válido!")
+    .required("Campo e-mail é obrigatório!"),
+  password: Yup.string()
+    .min(6, "A senha deve ter no mínimo 6 caracteres!")
+    .required("Campo senha é obrigatório!")
+});
 
 export default function SignUp() {
   return (
     <Container>
       <h1>DevSource</h1>
-      <Input placeholder="Seu nome de usuário do Github" type="text" />
-      <Input placeholder="Seu e-mail" type="email" />
-      <Input placeholder="Sua senha secreta" type="password" />
-      <Button>Cadastrar-se</Button>
+      <Formik
+        initialValues={{ username: "", email: "", password: "" }}
+        validationSchema={FormSchema}
+        onSubmit={values => {
+          alert(`${values.username}, ${values.email}, ${values.password}`);
+        }}
+      >
+        {({ handleChange, handleSubmit, errors }) => (
+          <>
+            <Input
+              placeholder="Seu nome de usuário do Github"
+              type="text"
+              onChange={handleChange("username")}
+            />
+            {errors.username && <MessageError>{errors.username}</MessageError>}
+            <Input
+              placeholder="Seu e-mail"
+              type="email"
+              onChange={handleChange("email")}
+            />
+            {errors.email && <MessageError>{errors.email}</MessageError>}
+            <Input
+              placeholder="Sua senha secreta"
+              type="password"
+              onChange={handleChange("password")}
+            />
+            {errors.password && <MessageError>{errors.password}</MessageError>}
+            <Button onClick={handleSubmit}>Cadastrar-se</Button>
+          </>
+        )}
+      </Formik>
       <Link to="/sign-in">Já tenho uma conta</Link>
     </Container>
   );
